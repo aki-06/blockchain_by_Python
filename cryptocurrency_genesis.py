507c57e82218c65c09c1173e802f1e25555b09ba
akihironemoto <akihiro.nemoto.06@gmail.com>
2017-11-10 22:03:28 +0900

    finished blockchain

--- cryptocurrency_genesis.py
+++ cryptocurrency_genesis.py
@@ -1,9 +1,8 @@
 from cryptocurrency_block import *
 import datetime as date
 
-
 def create_genesis_block():
-	"""
+    """
     manually creates a first block of a blockchain with index of 0 and
     arbitrary previous hash.
     first_block = create_genesis_block()
